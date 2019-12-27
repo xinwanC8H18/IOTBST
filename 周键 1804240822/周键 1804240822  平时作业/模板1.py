f0d26f28aa0caea61db94cde9c64e68b032c85b6
xinwanC8H18 <53950866+xinwanC8H18@users.noreply.github.com>
2019-12-27 15:32:33 +0800

    周键  1804240822
    平时作业与期末考试

---
+++ 周键 1804240822/周键 1804240822  平时作业/模板1.py
@@ -0,0 +1,12 @@
+{
+ "ret": 0,
+ "errmsg": "ok", # 请求状态
+ "msg_count": 10,  # 信息条数
+ "can_msg_continue": 1, # 是否还可以继续获取，1代表可以。0代表不可以，也就是最后一页
+ "general_msg_list": "{"list":[]}", # 公众号文本信息
+ "next_offset": 20, 
+ "video_count": 1,
+ "use_video_tab": 1,
+ "real_type": 0,
+ "home_page_list": []
+}
---
+++ 周键 1804240822/周键 1804240822  平时作业/爬虫 部分.py
@@ -0,0 +1,25 @@
+params = {
+        '__biz': biz,
+        'uin': uin,
+        'key': key,
+        'offset': offset,
+        'count': count,
+        'action': 'getmsg',
+        'f': 'json'
+    }
+ 
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'
+    }
+ 
+    response = requests.get(url=url, params=params, headers=headers)
+    resp_json = response.json()
+    if resp_json.get('errmsg') == 'ok':
+        resp_json = response.json()
+        # 是否还有分页数据， 用于判断return的值
+        can_msg_continue = resp_json['can_msg_continue']
+        # 当前分页文章数
+        msg_count = resp_json['msg_count']
+        general_msg_list = json.loads(resp_json['general_msg_list'])
+        list = general_msg_list.get('list')
+        print(list, "**************")
