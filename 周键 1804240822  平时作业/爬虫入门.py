11ed7d88f2fc8885a8d573d32f8e706e46bc6d93
xinwanC8H18 <53950866+xinwanC8H18@users.noreply.github.com>
2019-12-27 15:00:07 +0800

    周键   1804240822

---
+++ 周键 1804240822  平时作业/爬虫入门.py
@@ -0,0 +1,31 @@
+[{
+    "comm_msg_info": {
+        "id": 1000000038,
+        "type": 49,
+        "datetime": 1560474000,
+        "fakeid": "3881067844",
+        "status": 2,
+        "content": ""
+    },
+    "app_msg_ext_info": {
+        "title": "入门爬虫，这一篇就够了！！！",
+        "digest": "入门爬虫，这一篇就够了！！！",
+        "content": "",
+        "fileid": 0,
+        "content_url": "http:XXXXXX",
+        "source_url": "",
+        "cover": "I5kME6BVXeLibZDUhsiaEYiaX7zOoibxa9sb4stIwrfuqID5ttmiaoVAFyxKF6IjOCyl22vg8n2NPv98ibow\\/0?wx_fmt=jpeg",
+        "subtype": 9,
+        "is_multi": 0,
+        "multi_app_msg_item_list": [],
+        "author": "Python3X",
+        "copyright_stat": 11,
+        "duration": 0,
+        "del_flag": 1,
+        "item_show_type": 0,
+        "audio_fileid": 0,
+        "play_url": "",
+        "malicious_title_reason_id": 0,
+        "malicious_content_type": 0
+    }
+},{...},{...},{...},{...},{...},{...},{...},{...},{...}]
